1890998260e1ca7ade4d90772a3e828d32a7aa75
lee <inma8743@gmail.com>
2023-11-07 11:44:32 +0900

    cuda 테스트

--- cuda_test.py
+++ cuda_test.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
 # TensorFlow가 GPU를 인식하는지 확인합니다.
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+gpu_device_name = tf.test.gpu_device_name()
+if gpu_device_name:
+    gpu_confirmation = 'Default GPU Device: {}'.format(gpu_device_name)
 else:
-    print("TensorFlow GPU device not found")
+    gpu_confirmation = "TensorFlow GPU device not found"
 
 # 간단한 연산을 통해 GPU 사용을 확인합니다.
 with tf.device('/GPU:0'):
@@ -12,4 +13,8 @@ with tf.device('/GPU:0'):
     b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
     c = tf.matmul(a, b)
 
-print(c)
+# cudnn이 설치되어 있는지 확인합니다.
+cudnn_confirmation = tf.test.is_built_with_cuda()
+
+# 결과 출력
+gpu_confirmation, c.numpy(), cudnn_confirmation
